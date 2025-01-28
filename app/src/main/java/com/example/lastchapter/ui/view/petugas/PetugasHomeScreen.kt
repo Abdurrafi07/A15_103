d6b015b76d8f00bc1ca00c809b1186e414276db2
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-01-28 21:39:26 +0700

    Membuat Petugas Home Screen

---
+++ app/src/main/java/com/example/lastchapter/ui/view/petugas/PetugasHomeScreen.kt
@@ -0,0 +1,228 @@
+package com.example.lastchapter.ui.view.petugas
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.lastchapter.R
+import com.example.lastchapter.model.Petugas
+import com.example.lastchapter.ui.customwidget.CostumeTopAppBar
+import com.example.lastchapter.ui.navigation.DestinasiNavigasi
+import com.example.lastchapter.ui.viewmodel.PenyediaViewModel
+import com.example.lastchapter.ui.viewmodel.petugas.HomePetugasViewModel
+import com.example.lastchapter.ui.viewmodel.petugas.HomeoffUiState
+
+object DestinasiHomePetugas : DestinasiNavigasi {
+    override val route = "home_petugas"
+    override val titleRes = "Home Petugas"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePetugasScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    navigateBack: () -> Unit,
+    viewModel: HomePetugasViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomePetugas.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                onRefresh = { viewModel.getPetugas() },
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        }
+    ) { innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.offUIState,
+            retryAction = { viewModel.getPetugas() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deletePetugas(it.idPetugas)
+                viewModel.getPetugas()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeoffUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Petugas) -> Unit = {},
+    onDetailClick: (String) -> Unit
+) {
+    when (homeUiState) {
+        is HomeoffUiState.Loading -> {
+            OnLoading(modifier = modifier.fillMaxSize())
+        }
+        is HomeoffUiState.Success -> {
+            if (homeUiState.petugas.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Petugas")
+                }
+            } else {
+                PetugasLayout(
+                    petugas = homeUiState.petugas,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = { onDetailClick(it.idPetugas) },
+                    onDeleteClick = { onDeleteClick(it) }
+                )
+            }
+        }
+        is HomeoffUiState.Error -> {
+            OnError(retryAction, modifier = modifier.fillMaxSize())
+        }
+    }
+}
+
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun PetugasLayout(
+    petugas: List<Petugas>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Petugas) -> Unit,
+    onDeleteClick: (Petugas) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(petugas) { petugas ->
+            PetugasCard(
+                petugas = petugas,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(petugas) },
+                onDeleteClick = { onDeleteClick(petugas) }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun PetugasCard(
+    petugas: Petugas,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Petugas) -> Unit = {}
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Box(modifier = Modifier.padding(16.dp)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // Reorder the Text views to place idPetugas above namaPetugas
+                Text(
+                    text = petugas.idPetugas,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = petugas.namaPetugas,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Text(
+                    text = petugas.jabatan,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            IconButton(
+                onClick = { onDeleteClick(petugas) },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
